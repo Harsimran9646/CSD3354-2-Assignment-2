b814b9002483ef7296c44a6e35e3d88161b31067
visualstudio <visualstudio@230-PC13>
2019-03-07 16:02:37 -0500

    Lab6

--- C0707995_Lab1/Program.cs
+++ C0707995_Lab1/Program.cs
@@ -8,30 +8,42 @@ namespace C0707995_Assignment2
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        public static void Main()
         {
             //Student Name Harsimran Singh Student Id C0707995
             //CSD3354 Section 2 
             //Assignmnet 2
             //March 7,2019
-            DelegateExcercises a = new DelegateExcercises();
-            a.Method3();
+            DelegateExcercises delegateExcercises = new DelegateExcercises();
+            delegateExcercises.Method3();
+            Console.ReadLine();
         }
     }
 
 
     public class DelegateExcercises
     {
-        public delegate int MyDelegate();
+        public delegate int MyDelegate(int intValue);
 
-        void Method1()
+        int Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelegate");
+            return intMethod1 * 2;
+        }
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
         }
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
         }
     }

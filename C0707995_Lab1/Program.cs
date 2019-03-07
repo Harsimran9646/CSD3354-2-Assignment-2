a9c467b0a2ed901173602e488482dcdcd88943fa
visualstudio <visualstudio@230-PC13>
2019-03-07 14:23:13 -0500

    C0707995

--- C0707995_Lab1/Program.cs
+++ C0707995_Lab1/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0707995_Lab1
+namespace C0707995_Assignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Student Name Harsimran Singh Student Id C0707995
+            //CSD3354 Section 2 
+            //Assignmnet 2
+            //March 7,2019
+            DelegateExcercises a = new DelegateExcercises();
+            a.Method2();
         }
     }
+    
+    
+        public class DelegateExcercises
+        {
+            public delegate void MyDelegate();
+
+            void Method1()
+            {
+                Console.WriteLine("Method1");
+                Console.ReadLine();
+            }
+            public void Method2()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate();
+            }
+        }
+    
 }
